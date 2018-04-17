1888ad9f5d256878b3b81bb6ad729aa0e24fa115
Maria Posluk <marpo758@student.liu.se>
2018-04-17 22:18:45 +0200

    time delay calculated

--- Lab/Lab.m
+++ Lab/Lab.m
@@ -2,46 +2,62 @@
 %Maria Posluk, marpo758, 950310-0829
 
 
-%%
+%% Ta ut information om den mottagna signalen y(t)
 [y, fs] = audioread('signal-marpo758.wav');
 Ts = 1/fs;
 Y = fftshift(fft(y));
 Y = abs(Y);
 length_half = ceil(length(y)/2);
 freq = linspace(0, fs/2, length_half);
-%%
+%% Rita ut amplitudspektrat för Y(f)
 figure(1)
 hold on
-plot(freq, Y(1:length_half)); %36 kHz, 93 kHz, 150 kz
+plot(freq, Y(1:length_half)); %Möjliga bärfrekvenser: 36 kHz, 93 kHz, 150 kz
 title('Amplitudsspektrum'), xlabel('Frekvens [Hz]'), ylabel('|Y|');
 hold off
 
-%%
+%% Rita ut amplitudspektrat för Y(f) med normerad frekvens för att läsa av bandbredden för de tre möjliga bärfrekvenserna.
 norm_freq = linspace(0, 1, length_half);
 figure(2)
 hold on
 plot(norm_freq, Y(1:length_half)); %
 title('Amplitudsspektrum'), xlabel('Normerad frekvens'), ylabel('|Y|');
 hold off
-%%
-[B36,A36] = butter(9, [0.16, 0.20]); %hur vï¿½ljer man ordningen pï¿½ filtret?
+%% Filtrera kring de möjliga bärfrekvenserna med buttherworth 
+[B36,A36] = butter(9, [0.16, 0.20]); %ordning på filtret??
 [B93,A93] = butter(9, [0.44, 0.49]);
 [B150,A150] = butter(9, [0.73, 0.77]);
-%%
+
 y36 = filter(B36, A36, y);
 y93 = filter(B93, A93, y);
 y150 = filter(B150, A150, y);
-%%
+%% Räkna ut en tidsvektor för att kunna rita ut y(t)
 time = length(y)*Ts;
 t = (0:Ts:time-Ts);
-%%
+%% Rita ut y(t) kring de möjliga bärfrekvenserna
 figure(3)
 subplot(3, 1, 1);
 plot(t, y36);
+title('Vid frekvens 36 kHz'), xlabel('Tid [s]'), ylabel('y(t)');
 subplot(3, 1, 2);
 plot(t, y93);
+title('Vid frekvens 93 kHz'), xlabel('Tid [s]'), ylabel('y(t)');
 subplot(3, 1, 3);
 plot(t, y150);
+title('Vid frekvens 150 kHz'), xlabel('Tid [s]'), ylabel('y(t)');
+%I figur 3 kan man se att det är 36 kHz som är bärfrekvensen.
+
+%% Korrelation
+
+korr = xcorr(y36);
+
+figure(4)
+plot(korr); 
+title('Autokorrelation vid frekvens 36 kHz'), xlabel('Sampel'), ylabel('Korrelation');
+% topp till vänster: 5.032*10^6 sampel, topp i mitten: 5.2*10^6 sampel, topp till höger: 5.368*10^6 sampel.
+%Avstånd till mitten (5.368*10^6-5.032*10^6)/2= 1.68*10^5 sampel
+deltasampel = 1.68*10^5;
+Tau = deltasampel/fs; %Tau = 0.4200 s = 420 ms
 
 %xI = y * 2 * cos(2*pi*fc*t);
 %xQ = y * (-2) * sin(2*pi*fc*t);
